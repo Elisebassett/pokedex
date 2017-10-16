5138bfbf9eb317287b00c3e49e2703f7db4d9f0d
Elisebassett <elise@iileaders.com>
2017-10-16 15:28:44 -0600

    saving work

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 $(function () {
-	
-	$('#pokeload').on('click', function(){
-		for (var i = 0; i < pokemonArray.length; i++) {
-			$('#pokelist').append(`<button id=${pokemonArray[i]}>${pokemonArray[i]}</button>`);
-		}
-	});//pokelist/function
+
+	////Objects////
 
 	class Pokedex {
 		constructors() {
 			
 		}//constructors
 		showDetails(name){
-			cachedFetch(`http://pokeapi.co/api/v2/pokemon/${name}`)
-			.this(r=> r.json())
-			.this(results =>{
-				let images = results.message;
-				console.log(results.message);
-			});
+			cachedFetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+			.then(r=> r.json())
+			.then(results =>{
+				console.log(results);
+				let s = results.sprites;
+				let id = results.id;
+				let n = results.name;
+				let h = results.height;
+				let w = results.weight;
+				let tArray = [results.type];
+				let t = for (var i = 0; i < rArray.length; i++) {
+							return ${tArray[i]}
+						}//for loop
+
+
+
+				$('#display').append(`<img src="${s}"/> <h1>${id} ${n}</h1> <p>Height: ${h}</p> <p>Weight: ${w}</p> <p>Types: ${t}</p>`);
+			});//results
 		}//showDetails
-	}//pokedex
+	}//Pokedex
+
 	var pokedex = new Pokedex();
 
-	$('#test').click(function () {
-		pokedex.showDetails();
-	});
+	////Click events/////
+	
+	$('#pokeload').on('click', function(){
+		for (var i = 0; i < pokemonArray.length; i++) {
+			$('#pokelist').append(`<button id="${pokemonArray[i]}" data-name="${pokemonArray[i]}">${pokemonArray[i]}</button>`);
+		}//for loop
+	});//pokelist/function
 
-	// class Pokemon {
-	// 	constructors() {
-	// 		this.info = [i];
-	// 	}
-	// }
+	$(document).on('click', '.pokemon button', function () {
+		var name = $(this).attr('data-name');
+		pokedex.showDetails(name);
+	});//test function
+
+
+
+
 
 	
-	// var bulbasaur = new Pokemon();
 
 
 
