56436aa96122d2c47b43fb4aedc9791333238dea
Elisebassett <elise@iileaders.com>
2017-10-16 10:57:43 -0600

    inital boilerplate commit

---
+++ index.js
@@ -0,0 +1,63 @@
+$(function () {
+	
+	class Pokedex {
+		constructors() {
+
+		}
+		
+	}
+
+
+
+
+
+
+
+	// cachedFetch(url)
+	// .then(r=> r.jason())
+	// .then(results => {
+	// 	console.log(results)
+	//  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
